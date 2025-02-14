8e37e929ff924a8df9d075ae2352879a6b7ce1b6
Thejani2001 <thejanigamge16@gmail.com>
2025-02-14 22:25:35 +0530

    test

---
+++ app/Http/Controllers/frontrndcontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class frontrndcontroller extends Controller
+{
+    public function indexaboutus(){
+
+        return view('aboutus');
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/contact', function (){
+
+    return view('contactus');
+});
+
+
+Route::get('/about','frontrndcontroller@indexaboutus');
+
